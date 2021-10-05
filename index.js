4974f0b09dead7177021598d4a8d8c4a672bf10a
Denis Arce <dnsarce21@gmail.com>
2021-10-05 15:16:38 -0500

    wokring on employee data and writing html

--- index.js
+++ index.js
@@ -7,58 +7,182 @@ const jest = require("jest");
 const manager = require("./lib/Manager");
 const engineer = require("./lib/Engineer");
 const intern = require("./lib/Intern");
+const employee = require("./lib/Employee");
 
-const employee = new Employee
+// const employee = new Employee
 
 ///////Inquirer////////////////
+///// need to start with manager//////////
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Please enter employees name:",
-      name: "name",
-    },
+const managerInfo = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter Manager's name:",
+        name: "name",
+      },
 
-    {
-      type: "input",
-      message: "Please enter employees ID:",
-      name: "id",
-    },
+      {
+        type: "input",
+        message: "Please enter Manager's ID:",
+        name: "id",
+      },
 
-    {
-      type: "input",
-      message: "Please enter employees email:",
-      name: "email",
-    },
+      {
+        type: "input",
+        message: "Please enter Manager's email:",
+        name: "email",
+      },
 
-    {
-      type: "input",
-      message: "Please enter employees office number:",
-      name: "office",
-    },
+      {
+        type: "input",
+        message: "Please enter Manager's office number:",
+        name: "office",
+      },
 
-    {
-      type: "input",
-      message: "Please enter your gitHub username:",
-      name: "github",
-    },
+      // {
+      //   type: "input",
+      //   message: "Please enter your gitHub username:",
+      //   name: "github",
+      // },
 
-    {
-      type: "list",
-      name: "role",
-      choices: ["Engineer", "Intern", "Done adding Team Members"],
-    },
-  ])
-  .then(
-    async (answers) => await fs.writeFileSync("index.html", generateHTML(answers))
-  )
-  .then(() => console.log("HTML document succesfully created!"));
+      {
+        type: "list",
+        message: "Would you like to add another employee?",
+        name: "employee",
+        choices: ["Engineer", "Intern", "I am done adding Team Members"],
+      },
+    ])
+    // .then(
+    //   async (answers) => await fs.writeFileSync("index.html", generateHTML(answers))
+    // );
+
+    .then((answers) => {
+      managerData = `<div style="float:left; width: 300px; background-color: white; border-radius: 10px; box-shadow: 6px 6px gray; margin: 10px 10px 10px 10px;">
+        <p style="width:300px; position:relative; color:white; font-size:2rem; background-color: tomato; align-items: center; text-align: center; padding-bottom: 15px; padding-top: 10px; text-shadow: 1px 1px black; margin: 0%; border-radius: 10px 10px 0px 0px;" >${answers.name}</br> <span>&#9749;${answers.role}</span></p>
+        <div style="border-radius:10px; padding: 5px;">
+            <p type="text" style="width: 200px; padding-left: 5px;">ID: ${answers.id}</p>
+            <p type="text" style="width: 200px; padding-left: 5px;">Email: ${answers.email}</p>   
+            <p type="text" style="width: 200px; padding-left: 5px;">Office Number: ${answers.office}</p>     
+        </div>
+    </div>`;
+      addEmployee();
+    });
+};
+
+////////////////////////ENGINEER///////////////////////////////////
+
+const engineerInfo = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter employees name:",
+        name: "name",
+      },
+
+      {
+        type: "input",
+        message: "Please enter employees ID:",
+        name: "id",
+      },
+
+      {
+        type: "input",
+        message: "Please enter employees email:",
+        name: "email",
+      },
+
+      {
+        type: "input",
+        message: "Please enter your gitHub username:",
+        name: "github",
+      },
+    ])
+    .then((answers) => {
+      engineerData = `<div style="float:left; width: 300px; background-color: white; border-radius: 10px; box-shadow: 6px 6px gray; margin: 10px 10px 10px 10px;">
+      <p style="width:300px; position:relative; color:white; font-size:2rem; background-color: tomato; align-items: center; text-align: center; padding-bottom: 15px; padding-top: 10px; text-shadow: 1px 1px black; margin: 0%; border-radius: 10px 10px 0px 0px;" >${answers.name}</br> <span>&#9749;${answers.role}</span></p>
+      <div style="border-radius:10px; padding: 5px;">
+          <p type="text" style="width: 200px; padding-left: 5px;">ID: ${answers.id}</p>
+          <p type="text" style="width: 200px; padding-left: 5px;">Email: ${answers.email}</p>   
+          <p type="text" style="width: 200px; padding-left: 5px;">GitHub: ${answers.github}</p>     
+      </div>
+  </div>`;
+      addEmployee();
+    });
+};
+
+///////////////////////////INTERN/////////////////////////////////////////
+
+const interInfo = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter employees name:",
+        name: "name",
+      },
+
+      {
+        type: "input",
+        message: "Please enter employees ID:",
+        name: "id",
+      },
+
+      {
+        type: "input",
+        message: "Please enter employees email:",
+        name: "email",
+      },
+
+      {
+        type: "input",
+        message: "Please enter your school name:",
+        name: "school",
+      },
+    ])
+    .then((answers) => {
+      internData = `<div style="float:left; width: 300px; background-color: white; border-radius: 10px; box-shadow: 6px 6px gray; margin: 10px 10px 10px 10px;">
+      <p style="width:300px; position:relative; color:white; font-size:2rem; background-color: tomato; align-items: center; text-align: center; padding-bottom: 15px; padding-top: 10px; text-shadow: 1px 1px black; margin: 0%; border-radius: 10px 10px 0px 0px;" >${answers.name}</br> <span>&#9749;${answers.role}</span></p>
+      <div style="border-radius:10px; padding: 5px;">
+          <p type="text" style="width: 200px; padding-left: 5px;">ID: ${answers.id}</p>
+          <p type="text" style="width: 200px; padding-left: 5px;">Email: ${answers.email}</p>   
+          <p type="text" style="width: 200px; padding-left: 5px;">School Name: ${answers.school}</p>     
+      </div>
+  </div>`;
+      addEmployee();
+    });
+};
+
+/////////////////Adding a new employee//////////////
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "newItem",
+        message: "Would you like to add another employee",
+        choices: ["Engineer", "Intern", "I don't need to add more employees"],
+      },
+    ])
+
+    .then((answer) => {
+      if (answer.newItem === "Engineer") {
+        engineerInfo();
+      } else if (answer.newItem === "Intern") {
+        interInfo();
+      } else {
+        generateHTML();
+      }
+    });
+}
 
 /////Function to write HTML file////////////////
 
-const generateHTML = function (writeFile) {
-  const htmlDoc = ` <!DOCTYPE html>
+const generateHTML = function () {
+  htmlDoc = ` <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -88,32 +212,12 @@ const generateHTML = function (writeFile) {
                 </header>
           
                 <div style="display:inline-block; position: relative; margin-left: 250px;">
-                      <div style="float:left; width: 300px; background-color: white; border-radius: 10px; box-shadow: 6px 6px gray; margin: 10px 10px 10px 10px;">
-                          <p style="width:300px; position:relative; color:white; font-size:2rem; background-color: tomato; align-items: center; text-align: center; padding-bottom: 15px; padding-top: 10px; text-shadow: 1px 1px black; margin: 0%; border-radius: 10px 10px 0px 0px;" >Name</br> <span>&#9749;${writeFile.name}</span></p>
-                          <div style="border-radius:10px; padding: 5px;">
-                              <p type="text" style="width: 200px; padding-left: 5px;">ID: ${writeFile.id}</p>
-                              <p type="text" style="width: 200px; padding-left: 5px;">Email: ${writeFile.email}</p>   
-                              <p type="text" style="width: 200px; padding-left: 5px;">Office Number: ${writeFile.office}</p>     
-                          </div>
-                      </div>
+                      ${managerData}
           
-                      <div style="float:right; width: 300px; background-color: white; border-radius: 10px; box-shadow: 6px 6px gray; margin: 10px 10px 10px 10px;">
-                          <p style="width:300px; position:relative; color:white; font-size:2rem; background-color: tomato; align-items: center; text-align: center; padding-bottom: 15px; padding-top: 10px; text-shadow: 1px 1px black; margin: 0%; border-radius: 10px 10px 0px 0px;">Name</br> <span>&#128083;Engineer</span></p>
-                          <div style="padding: 5px; margin: 0px 0px 0px 10px; ">
-                              <p type="text" style="width: 200px; padding-left: 5px;">ID:</p>
-                              <p type="text" style="width: 200px; padding-left: 5px;">Email:</p>   
-                              <p type="text" style="width: 200px; padding-left: 5px;">Office Number:</p>   
-                          </div>
-                      </div>
-          
-                      <div style="float:right; width: 300px; background-color: white; border-radius: 10px; box-shadow: 6px 6px gray; margin: 10px 10px 10px 10px;">
-                          <p style="width:300px; position:relative; color:white; font-size:2rem; background-color: tomato; align-items: center; text-align: center; padding-bottom: 15px; padding-top: 10px; text-shadow: 1px 1px black; margin: 0%; border-radius: 10px 10px 0px 0px;" >Name</br> <span>&#128083;Engineer</span></p>
-                          <div style="padding: 5px;">
-                              <p type="text" style="width: 200px; padding-left: 5px;">ID:</p>
-                              <p type="text" style="width: 200px; padding-left: 5px;">Email:</p>   
-                              <p type="text" style="width: 200px; padding-left: 5px;">Office Number:</p>   
-                          </div>
-                      </div>  
+                      ${engineerData}
+                      
+                      ${internData}
+           
                 </div>
           
                 <div style="display:inline-block; position: inherit; margin-left: 400px;">
@@ -142,12 +246,9 @@ const generateHTML = function (writeFile) {
         </body>
         </html> `;
 
-  return htmlDoc;
+  fs.writeFile("index.html", htmlDoc, (err) => {
+    err ? console.error(err) : console.log("Succesfully wrote HTML doc");
+  });
 };
 
-
-/////// need to add function for each role////////////
-
-function managerRole() {
-
-}
+managerInfo();
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,6 +1,19 @@
 const Employee = require("../lib/Employee");
 
-describe("Employee", () => {
+// need to test employee class and input provided by user name-id-email//
+// does getRole() methof return role "employee"//
+// when a new object is created with name id email does it match with input//
 
+
+// describe("Employee", () => {
     
-})
+//     it("should return a new object with name-id-email", () => {
+
+//     const employee = new Employee( "Denis", 12345, "denisarce@me.com");
+    
+
+  
+//     });
+
+// }    
+
